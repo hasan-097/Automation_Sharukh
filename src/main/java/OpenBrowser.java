c96b97651d96a009640b6613a0fff9a6a5f476fa
hasan-097 <iamhasan097@gmail.com>
2024-12-15 19:21:54 +0530

    first commit

---
+++ src/main/java/OpenBrowser.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import org.junit.Assert; 
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+
+public class OpenBrowser {
+
+	@Test
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		ChromeDriver dr=new ChromeDriver();
+		dr.get("https://www.browserstack.com/");
+		dr.manage().window().maximize();
+		WebElement ele=dr.findElement(By.id("signupModalProductButton"));
+		Assert.isTrue(ele.isDisplayed(), "Not displayed");
+		Thread.sleep(10000);
+		dr.quit();
+
+	}
+
+}
